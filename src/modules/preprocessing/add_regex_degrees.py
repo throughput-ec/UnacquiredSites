3bd52941d76b6af07bdd1f169f6acae05bfb2add
Socorro <sedv8808@gmail.com>
2020-07-02 10:48:04 -0700

    added functions

--- src/modules/preprocessing/add_regex_degrees.py
+++ src/modules/preprocessing/add_regex_degrees.py
@@ -2,31 +2,28 @@
 
 dms_regex = r"([-]?\d{1,3}[°|′|\'|,]{0,3}\d{1,2}[,|′|\']{1,3}\d{0,2}[,|′|\']{1,4}[NESWnesw]?[\s|,|\']+?[-]?\d{1,3}[°|,|′|\']+\d{1,2}[,|′|\']+\d{0,2}[,|′|\'][,|′|\']{0,4}[NESWnesw]?)"
 dd_regex =  r"([-]?\d{1,3}\.\d{1,}[,]?[NESWnesw][\s|,|\']+?[-]?\d{1,3}\.\d{1,}[,]?[NESWnesw])"
-
+digits_regex = r"\d+"
 
 def convert_words_to_string(df, col_to_convert = 'col', new_col_name = 'words_as_string'):
     df[new_col_name] = df[col_to_convert].apply(lambda x: ','.join(map(str, x)))
     return df
 
 # Add columns to dataframe for the dms and dd REGEX
-def add_dms_re(df, search_col = 'col', new_col_name = 'dms_regex'):
+
+def find_re(df, find_val = 'dms_regex', search_col = 'col', new_col_name = 'dms_regex'):
     '''Add coordinates degree minutes second format
     Example:
     nlp_sentences = add_regex_degrees.add_dms_re(nlp_sentences, 'words_as_string', 'dms_regex')
     '''
-    df[new_col_name] = df[search_col].str.findall(dms_regex)
-    return df
-
-def add_dd_re(df, search_col = 'col', new_col_name = 'dd_regex'):
-    '''Add coordinates degree minutes second format
-    Example:
-    nlp_sentences = add_dd_re(nlp_sentences, 'words_as_string', 'dd_regex')
-    '''
-    df[new_col_name] = df[search_col].str.findall(dd_regex)
-    return df
-
-def find_digits_from_words(df, search_col = 'col', new_col_name = 'digits_from_words'):
-    df[new_col_name] = df[search_col].str.findall(r'\d+')
+    if find_val == 'dms_regex':
+        df[new_col_name] = df[search_col].str.findall(dms_regex)
+    
+    if find_val == 'dd_regex':
+        df[new_col_name] = df[search_col].str.findall(dd_regex)
+    
+    if find_val == 'digits_regex':
+        df[new_col_name] = df[search_col].str.findall(digits_regex)
+        
     return df
 
 def order_article(df, article_id, order_by = 'sentid', show_cols = 'words'):
@@ -41,3 +38,9 @@ def order_article(df, article_id, order_by = 'sentid', show_cols = 'words'):
     '''
     article = df[df['_gddid'] == article_id]
     return article[[order_by, show_cols]].sort_values(by = order_by)
+
+
+def find_intersections(df, cols_to_intersect = [], new_col_name = 'new_col'):
+    df[new_col_name] = df[cols_to_intersect].apply(lambda x: list(set.intersection(*map(set, list(x)))), axis = 1)
+
+    return df
