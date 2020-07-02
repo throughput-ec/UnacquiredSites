b3d125e9714f0a3b752f37731ac19608de4fed28
Socorro <sedv8808@gmail.com>
2020-07-02 12:27:19 -0700

    add module that can take care of all data

--- src/modules/preprocessing/preprocess_all_data.py
+++ src/modules/preprocessing/preprocess_all_data.py
@@ -8,5 +8,11 @@ def get_all_datasets():
     nlp_sentences = sentence_loader.preprocessed_sentences_sql()
     bibliography = bib_loader.preprocessed_bibliography()
     neotoma = nl.neotoma_loader()
+    neotoma_joined_df = nl.grouped_coords_df(neotoma)
 
-    return nlp_sentences, bibliography, neotoma
+    return nlp_sentences, bibliography, neotoma, neotoma_joined_df
+
+def get_nlp_bib_neotoma(nlp_sentences, bibliography, neotoma_joined_df):
+    nlp_bib = nlp_sentences.merge(bibliography, on = '_gddid')
+    nlp_bib_neotoma = nlp_bib.merge(neotoma_joined_df, on = 'doi', how = 'left')
+    return nlp_bib, nlp_bib_neotoma
