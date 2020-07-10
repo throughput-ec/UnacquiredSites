9861dc56759b34088e641d3c0e12a85220a2765a
Socorro <sedv8808@gmail.com>
2020-07-09 18:51:53 -0700

    arranged files a bit

--- src/tests/test_preprocess_all_data.py
+++ src/tests/test_preprocess_all_data.py
@@ -1,8 +1,5 @@
-import src.modules.preprocessing.nlp_sentence_loader as sentence_loader
-import src.modules.preprocessing.bibliography_loader as bib_loader
-import src.modules.preprocessing.add_regex_degrees as ard
-import src.modules.preprocessing.neotoma_loader as nl
-
+from src.modules.preprocessing.preprocess_all_data import *
+import pandas as pd
 
 def get_all_datasets():
     nlp_sentences = sentence_loader.preprocessed_sentences_sql()
