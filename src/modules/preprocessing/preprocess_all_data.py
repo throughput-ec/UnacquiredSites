73c2e48e084699e1f91c3534110464b3ace6b8cd
Simon Goring <goring@wisc.edu>
2020-08-03 13:03:27 -0700

    Mostly fixing line length.

--- src/modules/preprocessing/preprocess_all_data.py
+++ src/modules/preprocessing/preprocess_all_data.py
@@ -5,31 +5,35 @@ import utils as ard
 import neotoma_loader as nl
 
 # For Jupyter
-#import src.modules.preprocessing.nlp_sentence_loader as sentence_loader
-#import src.modules.preprocessing.bibliography_loader as bib_loader
-#import src.modules.preprocessing.utils as ard
-#import src.modules.preprocessing.neotoma_loader as nl
+# import src.modules.preprocessing.nlp_sentence_loader as sentence_loader
+# import src.modules.preprocessing.bibliography_loader as bib_loader
+# import src.modules.preprocessing.utils as ard
+# import src.modules.preprocessing.neotoma_loader as nl
 import argparse
-import sys
 import os
+import cProfile
+import pstats
+import io
 
-import cProfile, pstats, io
-
-## USAGE
-## python3 /Users/seiryu8808/Desktop/UWinsc/Github/UnacquiredSites/src/modules/preprocessing/preprocess_all_data.py \
-## --output_path='/Users/seiryu8808/Desktop' --output_name='preprocessed_sentences.tsv'
-## --bib_file='/Users/seiryu8808/Desktop/bibjson' --neotoma_file='/Users/seiryu8808/Desktop/data-1590729612420.csv'
+# USAGE
+# python3 /Users/seiryu8808/Desktop/UWinsc/Github/UnacquiredSites/src/modules/preprocessing/preprocess_all_data.py \
+# --output_path='/Users/seiryu8808/Desktop' --output_name='preprocessed_sentences.tsv'
+# --bib_file='/Users/seiryu8808/Desktop/bibjson' --neotoma_file='/Users/seiryu8808/Desktop/data-1590729612420.csv'
 
 path = r'/Users/seiryu8808/Desktop/UWinsc/Github/UnacquiredSites/src/output/for_model'
 bib_file = r'/Users/seiryu8808/Desktop/UWinsc/Github/Do_not_commit_data/bibjson'
 neotoma_file = r'/Users/seiryu8808/Desktop/UWinsc/Github/Do_not_commit_data/data-1590729612420.csv'
 
+
 def main():
     parser = argparse.ArgumentParser()
 
     parser.add_argument('--output_path', type=str, default=path,
-                        help='Directory where you want to output the preprocessed database.')
-    parser.add_argument('--output_name', type=str, default='preprocessed_sentences.tsv',
+                        help='Directory where you want to output the \
+                              preprocessed database.')
+    parser.add_argument('--output_name',
+                        type=str,
+                        default='preprocessed_sentences.tsv',
                         help='Name of the file as tsv.')
     parser.add_argument('--bib_file', type=str, default=bib_file,
                         help='Directory where bibliography json file is.')
@@ -39,23 +43,47 @@ def main():
 
     print("Loading all datasets")
 
-    nlp_sentences, bibliography, neotoma, neotoma_joined_df = get_all_datasets(bib_path = args.bib_file, neotoma_path = args.neotoma_file )
-    nlp_bib, nlp_bib_neotoma = get_nlp_bib_neotoma(nlp_sentences, bibliography, neotoma_joined_df)
+    nlp_sentences, bibliography, neotoma, neotoma_joined_df = get_all_datasets(bib_path=args.bib_file,
+                                                                               neotoma_path=args.neotoma_file)
+    nlp_bib, nlp_bib_neotoma = get_nlp_bib_neotoma(nlp_sentences,
+                                                   bibliography,
+                                                   neotoma_joined_df)
 
     print("...preprocessing data... please wait...")
 
-    nlp_bib_neotoma = ard.find_intersections(nlp_bib_neotoma, cols_to_intersect = ['digits_regex','longeast'], new_col_name = 'found_long')
-    nlp_bib_neotoma = ard.find_intersections(nlp_bib_neotoma, cols_to_intersect = ['digits_regex','latnorth'], new_col_name = 'found_lat')
-    nlp_bib_neotoma = ard.find_intersections(nlp_bib_neotoma, cols_to_intersect = ['words_l','sitenames_l'], new_col_name = 'found_sites')
+    nlp_bib_neotoma = ard.find_intersections(nlp_bib_neotoma,
+                                             cols_to_intersect=['digits_regex',
+                                                                'longeast'],
+                                             new_col_name='found_long')
+    nlp_bib_neotoma = ard.find_intersections(nlp_bib_neotoma,
+                                             cols_to_intersect=['digits_regex',
+                                                                'latnorth'],
+                                             new_col_name='found_lat')
+    nlp_bib_neotoma = ard.find_intersections(nlp_bib_neotoma,
+                                             cols_to_intersect=['words_l',
+                                                                'sitenames_l'],
+                                             new_col_name='found_sites')
 
-    nlp_bib_neotoma_for_model = nlp_bib_neotoma[['_gddid', 'digits_regex','sentid', 'words', 'words_as_string', 'link_url', 'found_lat', 'latnorth', 'found_long','longeast', 'found_sites']]
+    nlp_bib_neotoma_for_model = nlp_bib_neotoma[['_gddid',
+                                                 'digits_regex',
+                                                 'sentid',
+                                                 'words',
+                                                 'words_as_string',
+                                                 'link_url',
+                                                 'found_lat',
+                                                 'latnorth',
+                                                 'found_long',
+                                                 'longeast',
+                                                 'found_sites']]
 
     output_file = os.path.join(args.output_path, args.output_name)
-    nlp_bib_neotoma_for_model.to_csv(output_file, sep='\t', index = False)
+    nlp_bib_neotoma_for_model.to_csv(output_file, sep='\t', index=False)
 
-    print(f"You can find the preprocessed data in: {args.output_path}/{args.output_name}")
+    print(f"You can find the preprocessed data in: \
+            {args.output_path}/{args.output_name}")
 
-def get_all_datasets(bib_path = bib_file, neotoma_path = neotoma_file):
+
+def get_all_datasets(bib_path=bib_file, neotoma_path=neotoma_file):
     """
     Runs all data creator functions to get four main datasets
 
@@ -74,11 +102,12 @@ def get_all_datasets(bib_path = bib_file, neotoma_path = neotoma_file):
         pd.DataFrame with all Neotoma_Joined_DF database information
     """
     nlp_sentences = sentence_loader.preprocessed_sentences_sql()
-    bibliography = bib_loader.preprocessed_bibliography(path = bib_path)
-    neotoma = nl.neotoma_loader(file = neotoma_path)
+    bibliography = bib_loader.preprocessed_bibliography(path=bib_path)
+    neotoma = nl.neotoma_loader(file=neotoma_path)
     neotoma_joined_df = nl.grouped_coords_df(neotoma)
 
     return nlp_sentences, bibliography, neotoma, neotoma_joined_df
+
 
 def get_nlp_bib_neotoma(nlp_sentences, bibliography, neotoma_joined_df):
     """
@@ -96,13 +125,16 @@ def get_nlp_bib_neotoma(nlp_sentences, bibliography, neotoma_joined_df):
     Returns
     -------
     nlp_bib : pd.DataFrame
-        pd.DataFrame with summarized merged nlp sentences and bibliography information
+        pd.DataFrame with summarized merged nlp sentences and bibliography
+        information
     nlp_bib_neotoma : pd.DataFrame
-        pd.DataFrame with summarized NLP, Bibliography and Neotoma database information
+        pd.DataFrame with summarized NLP, Bibliography and Neotoma database
+        information
     """
-    nlp_bib = nlp_sentences.merge(bibliography, on = '_gddid')
-    nlp_bib_neotoma = nlp_bib.merge(neotoma_joined_df, on = 'doi')
+    nlp_bib = nlp_sentences.merge(bibliography, on='_gddid')
+    nlp_bib_neotoma = nlp_bib.merge(neotoma_joined_df, on='doi')
     return nlp_bib, nlp_bib_neotoma
+
 
 pr = cProfile.Profile()
 pr.enable()
