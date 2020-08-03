510ff66be2fa386b00a93c935fd23aa93df177b4
Simon Goring <goring@wisc.edu>
2020-08-03 11:35:02 -0700

    Removed absolute path from config.  Minor linting.

--- src/config.py
+++ src/config.py
@@ -1,7 +1,9 @@
-# This file was adapted from http://www.postgresqltutorial.com/postgresql-python/connect/
+# This file was adapted from
+#   http://www.postgresqltutorial.com/postgresql-python/connect/
 
-# The following config() function reads in the database.ini file and returns the connection
-# parameters as a dictionary. This function will be imported in to the main python script:
+# The following config() function reads in the database.ini file and
+# returns the connection parameters as a dictionary. This function will be
+# imported in to the main python script:
 
 #!/usr/bin/python
 from configparser import ConfigParser
@@ -22,8 +24,10 @@ def config(filename='/Users/seiryu8808/Desktop/UWinsc/Github/UnacquiredSites/src
         for param in params:
             db[param[0]] = param[1]
 
-    # Returns an error if a parameter is called that is not listed in the initialization file
+    # Returns an error if a parameter is called that is not listed in
+    #  the initialization file
     else:
-        raise Exception('Section {0} not found in the {1} file'.format(section, filename))
+        raise Exception('Section {0} not found in the {1} file'
+                        .format(section, filename))
 
     return db
