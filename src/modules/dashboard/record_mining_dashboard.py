0a7a20fd8c96eede1a0c1111e31e6a6d24f72b29
Socorro <sedv8808@gmail.com>
2020-08-23 21:58:18 -0700

    upload dash

--- src/modules/dashboard/record_mining_dashboard.py
+++ src/modules/dashboard/record_mining_dashboard.py
@@ -76,8 +76,21 @@ app.layout = html.Div(children=[html.H1('Record Mining Dashboard'),
                                 html.Div(children=[html.P('Choose a paper:'),
                                                    dcc.Dropdown(id="title_dropdown",
                                                                 options=options_list,
-                                                                value='Paradigms and proboscideans in the southern Great Lakes region, USA')]),
-                                                   html.Div(id='gddid_output', style={'whiteSpace': 'pre-line'}),
+                                                                value='Paradigms and proboscideans in the southern Great Lakes region, USA'),
+                                                    ]),
+                                html.Div(id='gddid_output', style={'whiteSpace': 'pre-line'}),
+
+                                dcc.Upload(id='upload-data',
+                                           children=html.Div(['Drag and Drop or ',
+                                                    html.A('Select Files')]),
+                                           style={'width': '100%',
+                                                  'height': '60px',
+                                                  'lineHeight': '60px',
+                                                  'borderWidth': '1px',
+                                                  'borderStyle': 'dashed',
+                                                  'borderRadius': '5px',
+                                                  'textAlign': 'center',
+                                                  'margin': '10px'}),
 
                                 dcc.Tabs(id='tabs-example', value='tab-1', children=[
                                                                                      dcc.Tab(label='Graphics', value='tab-1'),
@@ -282,6 +295,56 @@ def render_content(tab):
                                   html.Div(id='table_output_t2')
                                   ])
 
+
+
+# Weird tabledef parse_contents(contents, filename, date):
+    content_type, content_string = contents.split(',')
+
+    decoded = base64.b64decode(content_string)
+    try:
+        if 'csv' in filename:
+            # Assume that the user uploaded a CSV file
+            df = pd.read_csv(
+                io.StringIO(decoded.decode('utf-8')))
+        elif 'xls' in filename:
+            # Assume that the user uploaded an excel file
+            df = pd.read_excel(io.BytesIO(decoded))
+    except Exception as e:
+        print(e)
+        return html.Div([
+            'There was an error processing this file.'
+        ])
+
+    return html.Div([
+        html.H5(filename),
+        html.H6(datetime.datetime.fromtimestamp(date)),
+
+        dash_table.DataTable(
+            data=df.to_dict('records'),
+            columns=[{'name': i, 'id': i} for i in df.columns]
+        ),
+
+        html.Hr(),  # horizontal line
+
+        # For debugging, display the raw contents provided by the web browser
+        html.Div('Raw Content'),
+        html.Pre(contents[0:200] + '...', style={
+            'whiteSpace': 'pre-wrap',
+            'wordBreak': 'break-all'
+        })
+    ])
+
+
+@app.callback(Output('output-data-upload', 'children'),
+              [Input('upload-data', 'contents')],
+              [State('upload-data', 'filename'),
+               State('upload-data', 'last_modified')])
+def update_output(list_of_contents, list_of_names, list_of_dates):
+    if list_of_contents is not None:
+        children = [
+            parse_contents(c, n, d) for c, n, d in
+            zip(list_of_contents, list_of_names, list_of_dates)]
+        return children
 
 
 if __name__ == '__main__':
