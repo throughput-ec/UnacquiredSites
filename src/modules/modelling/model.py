b6419ff7b161c0b9c624310009fa59264471ad0a
Socorro <sedv8808@gmail.com>
2020-09-04 08:34:51 -0700

    updated df

--- src/modules/modelling/model.py
+++ src/modules/modelling/model.py
@@ -124,6 +124,21 @@ def main():
     train_pred_comp.to_csv(output_file2, sep='\t', index = False)
     print(f"Saving train labels - comparison dataframe for dashboard in: {output_file2}")
 
+
+    ## Getting a df with percentiles. Uncomment if you want to get this df again
+    '''
+    train_pred_comp_percentiles = pd.DataFrame(train_pred_comp['prediction_proba'])
+    test_pred_comp_percentiles = pd.DataFrame(test_pred_comp['prediction_proba'])
+    dataf = pd.concat([train_pred_comp_percentiles, test_pred_comp_percentiles])
+    dataf['prediction_proba'] = dataf['prediction_proba'].astype(str)
+    dataf['count'] = 1
+    
+    df = dataf.groupby(['prediction_proba'], as_index=False)['count'].count()
+    df['percentage'] = (df['count']/106640)*100
+    df = pd.DataFrame(df)
+    df.to_csv(r'src/output/eda/comparison_percentiles.tsv', sep='\t', index = False)
+    '''
+
 def prepare_data(file = in_file_name, validation_file = validation_file):
     input_file = file
 
