83f63c9eb653fa10d71c74fad720709cb5fa52f7
Socorro <sedv8808@gmail.com>
2021-01-18 17:59:04 -0800

    upgraded modelling module

--- src/modules/modelling/model.py
+++ src/modules/modelling/model.py
@@ -4,8 +4,6 @@ import random
 
 # Loading libraries for modeling
 from sklearn.naive_bayes import MultinomialNB
-from sklearn.tree import DecisionTreeClassifier
-from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.metrics import confusion_matrix, recall_score
 
@@ -23,20 +21,15 @@ import os
 import cProfile, pstats, io
 #warnings.filterwarnings("ignore")
 
-## USAGE
-## python3 src/modules/modelling/model.py \
-## --input_file = 'output/for_model/preprocessed_sentences.tsv' \
-## --trained_model='yes' --Svalidation_file='output/from_dashboard/last_validated.tsv'
+'''USAGE
+python3 src/modules/modelling/model.py \
+--input_file='output/preprocessed_data/preprocessed_sentences.tsv' \
+--eda_file='yes'
+'''
 
-#python3 src/modules/modelling/model.py --input_file='output/for_model/preprocessed_sentences.tsv' --trained_model='no'
-
-in_file_name = r'output/for_model/preprocessed_sentences.tsv'
-
-validation_file = r''
-
-out_file_name = r'output/predictions/comparison_file.tsv'
-
-out_dashboard_file_name = r'output/predictions/dashboard_file.tsv'
+in_file_name = r'output/preprocessed_data/preprocessed_sentences.tsv'
+out_file = r'output/predictions/predictions.tsv'
+eda_file = r'output/eda/probabilities_percentiles_df.tsv'
 
 def main():
     parser = argparse.ArgumentParser()
@@ -44,226 +37,110 @@ def main():
     parser.add_argument('--input_file', type=str, default=in_file_name,
                         help='Directory where your input data is.')
 
-    parser.add_argument('--validation_file', type=str, default=validation_file,
+    parser.add_argument('--output_file', type=str, default=out_file,
                         help='Directory where your input data is.')
 
-    parser.add_argument('--output_file', type=str, default=out_file_name,
-                        help='Directory where your input data is.')
-
-    parser.add_argument('--trained_model', type=str, default='yes',
-                        help='Use train model or no. Options: yes/no')
+    parser.add_argument('--eda_file', type=str, default='no',
+                        help='Do EDA files yes/no.')
 
     args = parser.parse_args()
 
 
-    X_train, y_train, X_test, y_test, data_test, data_train = prepare_data(file = args.input_file, validation_file = args.validation_file)
-    print("Using CountVectorizer to normalize data...")
+    X_train, y_train, dataframe = prepare_data(file = args.input_file)
+    print("Using CountVectorizer to preprocess data...")
 
-    # Start predictions
-    print("Getting ready for predictions...")
+    # Start training
+    clf = train(X_train, y_train)
+    print("Training model...")
 
-    if args.trained_model == 'yes':
-        print("Loading previous model")
-
-        y_pred, y_proba = predict(X_test, y_test, X_train, y_train, trained_model = 'yes')
-        print("Completed predictions")
-
-        y_train_pred, y_train_proba=predict_proba_train(X_train, y_train, trained_model = 'yes')
-
-    if args.trained_model == 'no':
-        print("Training again...")
-        y_pred, y_proba = predict(X_test, y_test, X_train, y_train, trained_model = 'no')
-        print("Getting predictions...")
-        print("Completed predictions!")
-
-        y_train_pred, y_train_proba=predict_proba_train(X_train, y_train, trained_model = 'no')
+    y_pred, y_proba = predictions(X_train, y_train)
+    print("Completed metrics")
 
     guessed_label = pd.DataFrame(y_pred)
-    actual_label = pd.DataFrame(y_test)
+    actual_label = pd.DataFrame(y_train)
     predicted_proba = pd.DataFrame(y_proba)
     actual_label = actual_label.reset_index()
 
-    original_sentence = pd.DataFrame(data_test[['gddid', 'title', 'sentid','sentence', 'found_lat', 'latnorth', 'found_long', 'longeast']])
+    original_sentence = pd.DataFrame(dataframe[['gddid', 'title', 'sentid','sentence', 'found_lat', 'latnorth', 'found_long', 'longeast']])
     original_sentence = original_sentence.reset_index()
 
-    test_pred_comp = guessed_label.join(actual_label)
-    test_pred_comp = test_pred_comp.drop(columns=['index'])
-    test_pred_comp.columns = ['predicted_label'] + test_pred_comp.columns.tolist()[1:]
+    prediction_comp = guessed_label.join(actual_label)
+    prediction_comp = prediction_comp.drop(columns=['index'])
+    prediction_comp.columns = ['predicted_label'] + prediction_comp.columns.tolist()[1:]
 
-    test_pred_comp = predicted_proba.join(test_pred_comp)
-    test_pred_comp.columns = ['prediction_proba'] + test_pred_comp.columns.tolist()[1:]
-    #test_pred_comp = test_pred_comp.drop(columns=['index'])
+    prediction_comp = predicted_proba.join(prediction_comp)
+    prediction_comp.columns = ['prediction_proba'] + prediction_comp.columns.tolist()[1:]
 
-    test_pred_comp = test_pred_comp.join(original_sentence)
-    test_pred_comp = test_pred_comp.drop(columns=['index'])
-    test_pred_comp = test_pred_comp.rename(columns={'0':'predicted_proba', 'has_both_lat_long_int':'original_label'})
-    #test_pred_comp.columns = ['predicted_proba'] + test_pred_comp.columns.tolist()[1:]
-    test_pred_comp['train/test'] = 'Test'
+    prediction_comp = prediction_comp.join(original_sentence)
+    prediction_comp = prediction_comp.drop(columns=['index'])
+    prediction_comp = prediction_comp.rename(columns={'0':'predicted_proba'})
+
+    prediction_comp['Train/Pred'] = 'Train'
     output_file = os.path.join(args.output_file)
-    test_pred_comp.to_csv(output_file, sep='\t', index = False)
+    prediction_comp.to_csv(output_file, sep='\t', index = False)
     print(f"Saving validation - prediction comparison dataframe in: {output_file}")
 
-    # Getting DF for Dashboard
-    guessed_train_label = pd.DataFrame(y_train_pred)
-    actual_train_label = pd.DataFrame(y_train)
-    predicted_train_proba = pd.DataFrame(y_train_proba)
-    actual_train_label = actual_train_label.reset_index()
+    ## Getting a df with percentiles.
+    if args.eda_file == 'yes':
+        print('Saving percentiles and metrics analysis in EDA folder')
+        percentiles = pd.DataFrame(prediction_comp['prediction_proba'])
 
-    original_train_sentence = pd.DataFrame(data_train[['gddid', 'title', 'sentid','sentence', 'found_lat', 'latnorth', 'found_long', 'longeast']])
-    original_train_sentence = original_train_sentence.reset_index()
+        bins = pd.cut(percentiles['prediction_proba'], [0.0, 0.125, 0.25, 0.50, 0.75, 1.0])
+        df = percentiles.groupby(bins)['prediction_proba'].agg(['count'])
+        df = df.reset_index()
+        df['perc'] = round(df['count']/df['count'].sum(),5)*100
 
-    train_pred_comp = guessed_train_label.join(actual_train_label)
-    train_pred_comp = train_pred_comp.drop(columns=['index'])
-    train_pred_comp.columns = ['predicted_label'] + train_pred_comp.columns.tolist()[1:]
+        df.to_csv(r'output/eda/comparison_percentiles.tsv', sep='\t', index = False)
 
-    train_pred_comp = predicted_train_proba.join(train_pred_comp)
-    train_pred_comp.columns = ['prediction_proba'] + train_pred_comp.columns.tolist()[1:]
-    #test_pred_comp = test_pred_comp.drop(columns=['index'])
-
-    train_pred_comp = train_pred_comp.join(original_train_sentence)
-    train_pred_comp = train_pred_comp.drop(columns=['index'])
-    train_pred_comp = train_pred_comp.rename(columns={'0':'predicted_proba', 'has_both_lat_long_int':'original_label'})
-    train_pred_comp['train/test'] = 'Train'
-    output_file2 = os.path.join(out_dashboard_file_name)
-    train_pred_comp.to_csv(output_file2, sep='\t', index = False)
-    print(f"Saving train labels - comparison dataframe for dashboard in: {output_file2}")
+        sys.stdout = open("output/eda/train_metrics.txt", "w")
+        print('NB-conf. matrix-from trained: \n\n', confusion_matrix(y_train, y_pred))
+        print('NB-recall-from trained:', recall_score(y_train, y_pred))
+        print('Check metrics milestones for test set in Notebooks')
+        sys.stdout.close()
 
 
-    ## Getting a df with percentiles. Uncomment if you want to get this df again
-    '''
-    train_pred_comp_percentiles = pd.DataFrame(train_pred_comp['prediction_proba'])
-    test_pred_comp_percentiles = pd.DataFrame(test_pred_comp['prediction_proba'])
-    dataf = pd.concat([train_pred_comp_percentiles, test_pred_comp_percentiles])
-    dataf['prediction_proba'] = dataf['prediction_proba'].astype(str)
-    dataf['count'] = 1
-
-    df = dataf.groupby(['prediction_proba'], as_index=False)['count'].count()
-    df['percentage'] = (df['count']/106640)*100
-    df = pd.DataFrame(df)
-    df.to_csv(r'output/eda/comparison_percentiles.tsv', sep='\t', index = False)
-    '''
-
-def prepare_data(file = in_file_name, validation_file = validation_file):
+def prepare_data(file = in_file_name):
     input_file = file
 
-    if validation_file==r'':
-        df = pd.read_csv(input_file, sep='\t')
-        df['validated_coordinates']=''
-        print("Did not use file with validated coordinates")
+    df = pd.read_csv(input_file, sep='\t')
+    print("Did not use file with validated coordinates")
 
-    else:
-        data = pd.read_csv(input_file, sep='\t')
-        validated_data = pd.read_csv(validation_file, sep='\t')
-        validated_data=validated_data[['gddid', 'sentid', 'validated_coordinates', 'timestamp']]
-        validated_data['validated_coordinates']=validated_data['validated_coordinates'].astype(int)
-        df=pd.merge(data, validated_data,  how='left', left_on=['gddid','sentid'], right_on = ['gddid','sentid'])
-        print("Used file with validated coordinates")
-
-
-
-    df['has_both_lat_long_int'] = ((df['found_lat'].apply(len) > 2) & (df['found_long'].apply(len) > 2 ))
+    df['true_label'] = ((df['found_lat'].apply(len) > 2) & (df['found_long'].apply(len) > 2 ))
 
     # Map True to One and False to Zero
-    df['has_both_lat_long_int'] = df['has_both_lat_long_int'].astype(int)
+    df['true_label'] = df['true_label'].astype(int)
 
-    df.loc[df['validated_coordinates'].isnull(),'validated_coordinates'] = df['has_both_lat_long_int']
-
-    # Balancing data - not sure this is a good idea as it takes away features
-    #random.seed(30)
-    #df0 = df[df['has_both_lat_long_int'] == 0]
-    #df0 = df0.sample(n = 600, random_state=1)
-    #df1 = df[df['has_both_lat_long_int'] == 1]
-    #df = pd.concat([df0, df1])
-
-    # Split data into training and testing sets
-    data_train, data_test = train_test_split(df, test_size = 0.20, random_state = 12)
-
-    # Translate words to vectors
-    # NLP model
+    # NLP Bag of Words
     vec = CountVectorizer(min_df=2)
 
     # Fit and transform training
-    if validation_file==r'':
-        X_train = vec.fit_transform(data_train['nltk'].fillna(' '))
-        y_train = data_train['has_both_lat_long_int']
+    X_train = vec.fit_transform(df['nltk'].fillna(' '))
+    y_train = df['true_label']
 
-        # Transform test data without fitting
-        X_test = vec.transform(data_test['nltk'].fillna(' '))
-        y_test = data_test['has_both_lat_long_int']
+    vec_model = 'output/count_vec_model.sav'
+    print('Trained CountVectorizer')
+    pickle.dump(vec, open(vec_model, 'wb'))
 
-        filename = 'output/count_vec_model.sav'
-        pickle.dump(vec, open(filename, 'wb'))
-
-        return X_train, y_train, X_test, y_test, data_test, data_train
-
-    else:
-        X_train = vec.fit_transform(data_train['nltk'].fillna(' '))
-        y_train = data_train['validated_coordinates']
-
-        filename = 'output/count_vec_model.sav'
-        pickle.dump(vec, open(filename, 'wb'))
-
-        # Transform test data without fitting
-        X_test = vec.transform(data_test['nltk'].fillna(' '))
-        y_test = data_test['validated_coordinates']
-
-        return X_train, y_train, X_test, y_test, data_test, data_train
-
-
-#X_train, y_train, X_test, y_test, data = prepare_data(path = path, data_file = data_file)
+    return X_train, y_train, df
 
 
 def train(X_train, y_train):
     clf = MultinomialNB()
-    #clf = DecisionTreeClassifier(min_samples_split = 40, max_depth = 12)
+    #clf = DecisionTreeClassifier(min_samples_split = 40, max_depth = 12) - first model
     clf.fit(X_train, y_train)
-    filename = 'output/finalized_model.sav'
+    filename = 'output/NB_model.sav'
     pickle.dump(clf, open(filename, 'wb'))
+    print('Trained NaiveBayes')
     return clf
 
+def predictions(X_train, y_train, eda = 'yes'):
+    loaded_model = pickle.load(open('output/NB_model.sav', 'rb'))
+    training_acc = loaded_model.score(X_train, y_train)
+    print(f"The model's training accuracy was of: {training_acc:.5f}")
+    y_pred = loaded_model.predict(X_train)
+    y_proba = loaded_model.predict_proba(X_train)[:,1]
 
-def predict(X_test, y_test, X_train, y_train, trained_model = 'yes'):
-    if trained_model == 'yes':
-        loaded_model = pickle.load(open('output/finalized_model.sav', 'rb'))
-        training_acc = loaded_model.score(X_train, y_train)
-        print(f"This model's training accuracy was of: {training_acc:.5f}")
-        result = loaded_model.score(X_test, y_test)
-        print(f"Model's validation accuracy: {result:.5f}")
-        y_pred = loaded_model.predict(X_test)
-        y_proba = loaded_model.predict_proba(X_test)[:,1]
-        print('conf. matrix', confusion_matrix(y_test, y_pred))
-        print('recall:', recall_score(y_test, y_pred))
-        return y_pred, y_proba
-
-    if trained_model == 'no':
-        print("Retraining the model")
-        model = train(X_train, y_train)
-        print("Updated final model.")
-        training_acc = model.score(X_train, y_train)
-        print(f"Model's training accuracy: {training_acc:.5f}")
-        result = model.score(X_test, y_test)
-        print(f"Model's validation accuracy: {result:.5f}")
-        y_pred = model.predict(X_test)
-        y_pred_train = model.predict(X_train)
-        y_proba = model.predict_proba(X_test)[:,1]
-        print('conf. matrix', confusion_matrix(y_test, y_pred))
-        print('recall:', recall_score(y_test, y_pred))
-
-        print('NB-conf. matrix-from trained', confusion_matrix(y_train, y_pred_train))
-        print('NB-recall-from trained:', recall_score(y_train, y_pred_train))
-        return y_pred, y_proba
-
-def predict_proba_train(X_train, y_train, trained_model = 'yes'):
-    if trained_model == 'yes':
-        loaded_model = pickle.load(open('output/finalized_model.sav', 'rb'))
-        y_train_pred = loaded_model.predict(X_train)
-        y_train_proba = loaded_model.predict_proba(X_train)[:,1]
-        return y_train_pred, y_train_proba
-    if trained_model == 'no':
-        model = train(X_train, y_train)
-        y_train_pred = model.predict(X_train)
-        y_train_proba = model.predict_proba(X_train)[:,1]
-        return y_train_pred, y_train_proba
+    return y_pred, y_proba
 
 # Uncomment for profiling
 #pr = cProfile.Profile()
